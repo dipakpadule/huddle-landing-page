d54f867edecd26e07184f197b3a908e6312872f1
dipak <sarkar3115@gmail.com>
2023-03-31 20:55:32 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import mockups from './images/illustration-mockups.svg';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className='huddle'>
+      <section className='hudle-logo'>
+      <img className='logo' src={logo} alt=''/>
+      <section className='mockups'>
+        <img src={mockups} alt=''/>
+      </section>
+      </section>
+      <section className='huddle-info'>
+        <h1>Build The Community Your Fans Will Love</h1>
+        <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
+        <button>Register</button>
+        <section className='icons'>
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-square-twitter"></i>
+          <i className="fa-brands fa-square-instagram"></i>
+        </section>
+      </section>
+    </article>
   );
 }
 
